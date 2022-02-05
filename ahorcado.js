98d695643e1d2f43fa56a2e614fde32654a8da5c
Finochio Adrian <eidrienhez33@gmail.com>
2022-02-05 10:16:26 -0300

    "Introducioendo logica para seleccionar palabra secreta"

--- ahorcado.js
+++ ahorcado.js
@@ -1,10 +1,18 @@
 // Inicialiazando el juego
 
 var botonJugar=document.querySelector("#iniciar-juego");
-
+var palabrasSecretas=['ALURA','ORACLE','JAVASCRIPT','FUNCION','ARREGLO','CICLO','CASA','COLEGIO','ESTUDIOS','FUTBOL','VACACIONES','DEPORTES','AMIGOS','FAMILIA']
 botonJugar.addEventListener("click",function(event){
     event.preventDefault();
-    
+    var palabraSecreta=sortearPalabraSecreta(palabrasSecretas);
+    console.log(palabraSecreta);
     dibujarTablero();
     
 })
+
+function sortearPalabraSecreta(array){
+let indiceSorteado=Math.floor(Math.random()*(array.length))
+return array[indiceSorteado];
+
+}
+
