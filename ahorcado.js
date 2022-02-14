123acf17c872ca44525d1be91e5de9ab9747424e
Finochio Adrian <eidrienhez33@gmail.com>
2022-02-14 11:49:39 -0300

    añadiendo lógica funciones verificacion de ganasdor o fin del Juego, + Lógica botonAgregarPalabaras y verificacion de que sea mayúsculas

--- ahorcado.js
+++ ahorcado.js
@@ -1,26 +1,49 @@
 // Inicialiazando el juego
-var intento = 0;
+var intento;
 var palabraSecreta;
 var palabraSecretaArray;
 var botonJugar = document.querySelector("#iniciar-juego");
 var palabrasSecretas = ['ALURA', 'ORACLE', 'JAVASCRIPT', 'FUNCION', 'ARREGLO', 'CICLO', 'CASA', 'COLEGIO', 'ESTUDIOS', 'FUTBOL', 'VACACIONES', 'DEPORTES', 'AMIGOS', 'FAMILIA']
 var body = document.querySelector("body");
+var letrasPresionadas = [];
+var aciertos;
+var botonAgregarPalabras = document.querySelector("#nueva-palabra");
+
+
+botonAgregarPalabras.addEventListener("click", (event) => {
+
+    event.preventDefault();
+    var inputPalabraNueva = document.querySelector("#input-nueva-palabra");
+    var nuevaPalabra = inputPalabraNueva.value;
+    if (validarPalabraEnMayusculas(nuevaPalabra)) {
+        console.log(nuevaPalabra);
+        palabrasSecretas.push(nuevaPalabra);
+        console.log(palabrasSecretas);
+
+    }
+
+
+})
 botonJugar.addEventListener("click", function (event) {
 
     event.preventDefault();
     intento = 0;
+    aciertos = 0;
     palabraSecreta = crearPalabraSecreta(palabrasSecretas);
-
+    letrasPresionadas.length = 0;
     console.log(palabraSecreta);
     dibujarTablero();
     dibujarGuiones(palabraSecreta.length);
     palabraSecretaArray = palabraSecreta.split("");
+    reiniciarPosicionLetrasIncorrectas();
     console.log(palabraSecretaArray);
     dibujarAhorcado(intento);
 
 
 
+    console.log(palabraSecreta.length);
     body.addEventListener("keydown", (event) => evaluarEntradaTeclado(event));
+
 
 })
 
@@ -35,30 +58,47 @@ function evaluarEntradaTeclado(event) {
     var keycode = event.keyCode;
     var keys = event.key;
 
-    if ((keycode > 64 && keycode < 91) || keycode == 192) {
-        if (validarLetraMayuscula(keys)) {
-            if (palabraSecretaArray.includes(keys)) {
+    if (!finDelJuego(intento) && !ganastes(aciertos, palabraSecreta)) {
+        if ((keycode > 64 && keycode < 91) || keycode == 192) {
+            if (validarLetraMayuscula(keys)) {
+                if (palabraSecretaArray.includes(keys) && !letrasPresionadas.includes(keys)) {
 
-                for (i = 0; i < palabraSecretaArray.length; i++) {
+                    for (i = 0; i < palabraSecretaArray.length; i++) {
 
-                    if (palabraSecretaArray[i]==keys){
-                        dibujarLetraCorrecta(keys, i+1, palabraSecreta.length);
+                        if (palabraSecretaArray[i] == keys) {
+
+                            dibujarLetraCorrecta(keys, i, palabraSecreta.length);
+                            aciertos++;
+                            console.log(aciertos);
+                            if (ganastes(aciertos, palabraSecreta)) {
+                                dibujarGanastes();
+                            }
+                        }
+
                     }
+                    letrasPresionadas.push(keys);
 
-                };
 
+                } else if (!letrasPresionadas.includes(keys)) {
 
-            } else{
-                intento++;
-                dibujarAhorcado(intento);
-                
+                    intento++;
+                    dibujarAhorcado(intento);
+
                     dibujarLetraIncorrecta(keys);
-                
-            }
+                    letrasPresionadas.push(keys);
 
-        }
+
+                }
+
+
+            }
+        };
+    } else if (finDelJuego(intento)) {
+        dibujarFinDelJuego();
+
     }
-};
+
+}
 
 function validarLetraMayuscula(tecla) {
     let regxp = new RegExp(/[A-Z]|[Ñ]/);
@@ -67,3 +107,24 @@ function validarLetraMayuscula(tecla) {
 
 }
 
+function finDelJuego(intento) {
+    return (intento == 9) ? true : false;
+}
+
+function ganastes(aciertos, str) {
+    return (aciertos == str.length) ? true : false;
+}
+
+function validarPalabraEnMayusculas(palabra) {
+    let arrayPalabraInsertada = palabra.split("");
+    let estado = true;
+    
+    arrayPalabraInsertada.forEach(element => {
+        let letra = validarLetraMayuscula(element)
+        if (!letra) {
+            estado = false;
+        }
+    });
+    
+    return estado;
+}
